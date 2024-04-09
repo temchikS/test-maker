02791808ed55dbe293c4edb62722da18c45a8a66
temchikS <148534802+temchikS@users.noreply.github.com>
2024-04-09 23:43:30 +0600

    Delete frontend/src/components/Welcome.jsx

--- frontend/src/components/Welcome.jsx
+++
@@ -1,9 +0,0 @@
-export default function WelcomePage(){
-    return(
-        <div className='login-container'>
-        <div className='background'>
-            <h1>WELCOME!!!!!!!!!</h1>   
-        </div>
-        </div>
-    );
-}
